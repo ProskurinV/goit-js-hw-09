d87b2c37d792d366db4c65dc58e0973858b87a23
ProskurinV <97758596+ProskurinV@users.noreply.github.com>
2022-08-09 19:17:13 +0000

    Deploying to gh-pages from @ ProskurinV/goit-js-hw-09@1d4527c35a04123cfe23b0fb81f46af1d7f0ac90 🚀

---
+++ 01-color-switcher.4a634e2f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let l=null;e.addEventListener("click",(function(){l=setInterval((()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16)}`;d.style.backgroundColor=e}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(l),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.4a634e2f.js.map
