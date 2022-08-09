1d4527c35a04123cfe23b0fb81f46af1d7f0ac90
ProskurinV <proskyrinv.g@gmail.com>
2022-08-09 22:16:37 +0300

    1task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }, 1000);
+
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
